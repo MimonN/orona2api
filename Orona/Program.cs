dda5511787b0d98cfa1191ca094360214d0dd733
MimonN <m.a.nedilsky@gmail.com>
2023-01-28 10:57:20 -0500

    publish version

--- Orona/Program.cs
+++ Orona/Program.cs
@@ -109,6 +109,18 @@ app.UseHttpsRedirection();
 
 app.UseCors("CorsPolicy");
 
+app.Use(async (context, next) =>
+{
+    await next();
+
+    if(context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+    {
+        context.Request.Path = "/index.html";
+        await next();
+    }
+});
+
+app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions()
 {
